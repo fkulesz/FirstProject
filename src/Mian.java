6fd79bee70999e70ed4e2df907b09c6712b38225
fkulesz <fkulesz@poczta.onet.pl>
2020-01-16 10:13:41 +0100

    First Commit

---
+++ src/Mian.java
@@ -0,0 +1,7 @@
+public class Mian {
+    public static void main(String[] args) {
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
+    }
+}
